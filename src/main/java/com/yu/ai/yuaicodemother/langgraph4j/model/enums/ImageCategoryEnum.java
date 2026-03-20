338c21ed89b59395994d773b52167ee2ff386d19
Lzh-hub-theo <3039029928@qq.com>
2026-03-20 14:19:02 +0800

    教程第10期 - AI工作流（编写节点）
    ## 在上一步的基础上，为工作流中每一个节点单独定义一个类。
    ## 为了兼容旧逻辑，定义SpringContextUtil工具类静态获取Bean
    ## 定义了图片分类枚举，还有图片资源包装类
    ## 编写4个图片收集工具类给ai工具调用
    ## 编写ai收集图片提示词，创建ai服务与工厂类
    ## 编写ai收集图片节点、增强提示词节点、路由节点等

---
+++ src/main/java/com/yu/ai/yuaicodemother/langgraph4j/model/enums/ImageCategoryEnum.java
@@ -0,0 +1,37 @@
+package com.yu.ai.yuaicodemother.langgraph4j.model.enums;
+
+import cn.hutool.core.util.ObjUtil;
+import lombok.Getter;
+
+@Getter
+public enum ImageCategoryEnum {
+    CONTENT("内容图片", "CONTENT"),
+    LOGO("LOGO图片", "LOGO"),
+    ILLUSTRATION("插画图片", "ILLUSTRATION"),
+    ARCHITECTURE("架构图片", "ARCHITECTURE");
+
+    private final String text;
+    private final String value;
+
+    ImageCategoryEnum(String text, String value){
+        this.text = text;
+        this.value = value;
+    }
+
+    /**
+     * 根据value获取枚举
+     * @param value
+     * @return
+     */
+    public static ImageCategoryEnum getEnumByValue(String value) {
+        if(ObjUtil.isEmpty(value)){
+            return null;
+        }
+        for (ImageCategoryEnum roleEnum : ImageCategoryEnum.values()) {
+            if (roleEnum.getValue().equals(value)) {
+                return roleEnum;
+            }
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/yu/ai/yuaicodemother/langgraph4j/node/RouterNode.java
@@ -0,0 +1,37 @@
+package com.yu.ai.yuaicodemother.langgraph4j.node;
+
+import cn.hutool.extra.spring.SpringUtil;
+import com.yu.ai.yuaicodemother.ai.AiCodeGenTypeRoutingService;
+import com.yu.ai.yuaicodemother.langgraph4j.state.WorkflowContext;
+import com.yu.ai.yuaicodemother.model.enums.CodeGenTypeEnum;
+import lombok.extern.slf4j.Slf4j;
+import org.bsc.langgraph4j.action.AsyncNodeAction;
+import org.bsc.langgraph4j.prebuilt.MessagesState;
+
+import static org.bsc.langgraph4j.action.AsyncNodeAction.node_async;
+
+@Slf4j
+public class RouterNode {
+    public static AsyncNodeAction<MessagesState<String>> create() {
+        return node_async(state -> {
+            WorkflowContext context = WorkflowContext.getContext(state);
+            log.info("执行节点: 智能路由");
+
+            CodeGenTypeEnum generationType;
+            try{
+                AiCodeGenTypeRoutingService aiCodeGenTypeRoutingService = SpringUtil.getBean(AiCodeGenTypeRoutingService.class);
+                generationType = aiCodeGenTypeRoutingService.routeCodeGenType(context.getOriginalPrompt());
+                log.info("AI 智能路由完成，选择类型：{}（{}）",generationType.getValue(),generationType.getText());
+            }catch (Exception e){
+                log.error("AI 智能路由失败，使用默认的HTML：{}", e.getMessage());
+                generationType=CodeGenTypeEnum.HTML;
+            }
+
+            // 更新状态
+            context.setCurrentStep("智能路由");
+            context.setGenerationType(generationType);
+            log.info("路由决策完成，选择类型: {}", generationType.getText());
+            return WorkflowContext.saveContext(context);
+        });
+    }
+}
