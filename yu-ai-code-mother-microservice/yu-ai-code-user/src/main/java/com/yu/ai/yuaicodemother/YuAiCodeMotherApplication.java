4a20027f6aa32526e597198d5900389f6de8a5d0
Lzh-hub-theo <3039029928@qq.com>
2026-03-27 19:15:12 +0800

    教程第14期 - 微服务改造 - 应用服务模块
    1，中间态

---
+++ yu-ai-code-mother-microservice/yu-ai-code-user/src/main/java/com/yu/ai/yuaicodemother/YuAiCodeMotherApplication.java
@@ -0,0 +1,15 @@
+package com.yu.ai.yuaicodemother;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.yu.ai.yuaicodemother.mapper")
+public class YuAiCodeMotherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuAiCodeMotherApplication.class, args);
+    }
+
+}
