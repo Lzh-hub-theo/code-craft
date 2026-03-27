8299f7fdbd81178d5436dd5763b18abd2ee5aad9
Lzh-hub-theo <3039029928@qq.com>
2026-03-27 20:05:18 +0800

    教程第14期 - 微服务改造 - ai模块
    1，中间态
    2，与应用模块强绑定的ai代码放在应用模块中

---
+++ yu-ai-code-mother-microservice/yu-ai-code-ai/src/main/java/com/yu/ai/yuaicodemother/ai/AiCodeGeneratorService.java
@@ -0,0 +1,53 @@
+package com.yu.ai.yuaicodemother.ai;
+
+import com.yu.ai.yuaicodemother.ai.model.HtmlCodeResult;
+import com.yu.ai.yuaicodemother.ai.model.MultiFileCodeResult;
+import dev.langchain4j.service.MemoryId;
+import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.TokenStream;
+import dev.langchain4j.service.UserMessage;
+import reactor.core.publisher.Flux;
+
+public interface AiCodeGeneratorService {
+
+    /**
+     * 生成HTML代码
+     * @param userMessage 用户消息
+     * @return 生成的代码结果
+     */
+    @SystemMessage(fromResource = "prompt/codegen-html-system-prompt.txt")
+    HtmlCodeResult generateHtmlCode(String userMessage);
+
+    /**
+     * 生成多文件代码
+     * @param userMessage
+     * @return
+     */
+    @SystemMessage(fromResource = "prompt/codegen-multi-file-system-prompt.txt")
+    MultiFileCodeResult generateMultiFileCode(String userMessage);
+
+    /**
+     * 生成HTML代码（流式）
+     * @param userMessage
+     * @return
+     */
+    @SystemMessage(fromResource = "prompt/codegen-html-system-prompt.txt")
+    Flux<String> generateHtmlCodeStream(String userMessage);
+
+    /**
+     * 生成多文件代码（流式）
+     * @param userMessage
+     * @return
+     */
+    @SystemMessage(fromResource = "prompt/codegen-multi-file-system-prompt.txt")
+    Flux<String> generateMultiFileCodeStream(String userMessage);
+
+    /**
+     * 生成vue项目代码（流式）
+     * @param appId 应用ID
+     * @param userMessage 用户消息
+     * @return 生成的流式响应
+     */
+    @SystemMessage(fromResource = "prompt/codegen-vue-project-system-prompt.txt")
+    TokenStream generateVueProjectCodeStream(@MemoryId long appId, @UserMessage String userMessage);
+}
