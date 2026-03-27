606bcb1b014082d0e31181b9d1a52e8962fe256f
Lzh-hub-theo <3039029928@qq.com>
2026-03-27 14:28:51 +0800

    教程第14期 - 微服务改造 - 通用模块
    1，中间态

---
+++ yu-ai-code-mother-microservice/yu-ai-code-common/src/main/java/com/yu/ai/yuaicodemother/exception/ThrowUtils.java
@@ -0,0 +1,26 @@
+package com.yu.ai.yuaicodemother.exception;
+
+public class ThrowUtils {
+    /**
+     * 条件成立则抛异常
+     * @param condition
+     * @param runtimeException
+     */
+    public static void throwIf(boolean condition, RuntimeException runtimeException){
+        if(condition){
+            throw runtimeException;
+        }
+    }
+
+    public static void throwIf(boolean condition, ErrorCode errorCode){
+        if(condition){
+            throw new BusinessException(errorCode);
+        }
+    }
+
+    public static void throwIf(boolean condition, ErrorCode errorCode, String message){
+        if(condition){
+            throw new BusinessException(errorCode,message);
+        }
+    }
+}
